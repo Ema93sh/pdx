f7c917aa4b5807d3a5529ba8ed31af00101a9e84
Anurag Koul <koulanurag@gmail.com>
2018-04-01 14:50:11 -0700

    added predicted delta explanations

--- explanations/explanations.py
+++ explanations/explanations.py
@@ -1,6 +1,31 @@
+import numpy as np
+
 
 class Explanation(object):
     """Explanation class related to generating explanations"""
+
     def __init__(self):
         super(Explanation, self).__init__()
         # TODO
+
+    def get_pdx(self, q_values, selected_action, target_actions):
+        """predicted delta explanations for the selected action in comparision with the target action.
+
+        :param list q_values: decomposed q-values
+        :param int selected_action:
+        :param list target_actions:
+
+        >>> Explanation().get_pdx(np.array([[1, 1, 1, 1], [4, 3, 2, 1]]), 0, [1, 2])
+        [[0, 0], [1, 2]]
+
+        """
+        pdx = [[(q_values[r][selected_action] - q_values[r][target]) for target in target_actions]
+               for r in range(len(q_values))]
+
+        return pdx
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod(verbose=True)
