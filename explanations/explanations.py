3d1af74b33221b52086396e5ec5f547eaa01a005
Anurag Koul <koulanurag@gmail.com>
2018-04-01 16:13:45 -0700

    added contribution aspect in predicted delta explanations

--- explanations/explanations.py
+++ explanations/explanations.py
@@ -16,13 +16,22 @@ class Explanation(object):
         :param list target_actions:
 
         >>> Explanation().get_pdx(np.array([[1, 1, 1, 1], [4, 3, 2, 1]]), 0, [1, 2])
-        [[0, 0], [1, 2]]
+        ([[0, 0], [1, 2]], [[0.0, 0.0], [1.0, 1.0]])
+
+        >>> Explanation().get_pdx(np.array([[1, 0, 2, 1], [3, 1, 2, 0]]), 0, [1, 2])
+        ([[1, -1], [2, 1]], [[0.33, 0.0], [0.67, 1.0]])
 
         """
         pdx = [[(q_values[r][selected_action] - q_values[r][target]) for target in target_actions]
                for r in range(len(q_values))]
 
-        return pdx
+        _pdx = np.array(pdx.copy())
+        _pdx[_pdx < 0] = 0
+        _atomic_pdx = np.array(_pdx).sum(axis=0)
+        contribution = [[round(_pdx[r][target] / (_atomic_pdx[target] + 0.001), 2)
+                         for target in range(len(target_actions))]
+                        for r in range(len(q_values))]
+        return pdx, contribution
 
 
 if __name__ == '__main__':
