7796849c36f6ee9a3e4d3b56c77c9477d87e3b22
Ema93sh <m.magesh.66@gmail.com>
2018-04-02 18:34:03 -0700

    Ground Truth for a state

--- explanations/explanations.py
+++ explanations/explanations.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import torch
+import copy
+from torch.autograd import Variable
 
 class Explanation(object):
     """Explanation class related to generating explanations"""
@@ -32,6 +34,40 @@ class Explanation(object):
                          for target in range(len(target_actions))]
                         for r in range(len(q_values))]
         return pdx, contribution
+
+    def get_gtx(self, env, model, state_config, action_space, episodes = 1):
+        """Estimate the ground truth for state and actions"""
+
+        expected_q_values = []
+        for episode in range(episodes):
+            current_config = copy.deepcopy(state_config)
+            state = env.reset(**current_config)
+            state = Variable(torch.Tensor(state.tolist())).unsqueeze(0)
+            episode_reward = []
+        
+            for action in range(action_space):
+                rewards = []
+                state, reward, done, info = env.step(action)
+                rewards.append(reward)
+
+                while not done:
+                    state = Variable(torch.Tensor(state.tolist())).unsqueeze(0)
+                    cominded_q_values, q_values = model(state)
+                    q_values = q_values.squeeze(1).data.numpy()
+                    action = int(cominded_q_values.data.max(1)[1])
+
+                    state, reward, done, info = env.step(action)
+                    rewards.append(reward)
+
+                rewards = np.stack(rewards)
+                episode_reward.append(rewards.sum(0))
+
+            expected_q_values.append(episode_reward)
+        expected_q_values = np.stack(expected_q_values, 1)
+
+        q_values = expected_q_values.mean(1)
+
+        return q_values
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import torch
 import os
 import visdom
 from envs import FruitCollection1D, FruitCollection2D
+
 from models.q_model import QModel
 from models.decomposed_q_model import DecomposedQModel
 from utils.task_runner import TaskRunner
 from utils.decomposed_task_runner import DecomposedQTaskRunner
+from explanations import Explanation
 
+from torch.autograd import Variable
 
 def get_env(args, viz):
     env_map = {
@@ -109,3 +112,17 @@ if __name__ == '__main__':
         task_runner.train(training_episodes=args.train_episodes)
 
     task_runner.test(test_episodes=args.test_episodes, render=args.render, sleep=args.sleep)
+
+
+    # explanation = Explanation()
+    # state_config = {
+    #     "fruits_loc": [1],
+    #     "step_count": 0,
+    #     "agent_position": [0, 4],
+    #     "score": 0,
+    #     "step_reward": 0,
+    #     "fruit_collected": 0
+    # }
+    #
+    # gtx = explanation.get_gtx(env, model, state_config, env.action_space, episodes = 100)
+    # print(gtx)
